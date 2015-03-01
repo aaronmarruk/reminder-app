703f1b62123253281ce472e1c2773ffcc13c9386
aaronmarruk <aaronmarruk@gamil.com>
2015-03-01 14:59:10 +0000

    Fix 24 clock bug

--- server/syncedCron.js
+++ server/syncedCron.js
@@ -21,13 +21,19 @@ Meteor.methods({
 				// There's a bug with later.js when there are no dates in 
 				// future schedule, so we'll repeat every year and then 
 				// immediately remove the job once it's complete
+				var hour = '';
+				if(dateArray[5] === 'AM'){
+			      	hour = dateArray[3]*1
+			    } else {
+			      	hour = dateArray[3]*1+12
+			    }
 				return parser.recur().every(1).year().on(dateArray[0]*1)
 				  	.month()
 			      	.on(dateArray[1]*1)
 			      	.dayOfMonth()
 			      	.on(dateArray[2]*1)
 			      	.year()
-			      	.on(dateArray[3]*1)
+			      	.on(hour)
 			      	.hour()
 			      	.on(dateArray[4]*1)
 			      	.minute();

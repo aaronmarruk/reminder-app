b78921dce68d9f9db9be96291ed89284b40c43be
aaronmarruk <aaronmarruk@gamil.com>
2015-03-07 23:18:47 +0000

    Tweak logging routing

--- client/templates/accounts/logout.js
+++ client/templates/accounts/logout.js
@@ -8,8 +8,11 @@ Template.logout.events({
       // The user might not have been found, or their passwword
       // could be incorrect. Inform the user that their
       // login attempt has failed. 
+
           } else {
             // The user has been logged in.
+            //FIXME do we really need to do this?
+            Router.go('splashPage');
           }
         });
         return false; 
